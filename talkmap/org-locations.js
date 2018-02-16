dec46c7392f970f1b1ee96053bd29019a2656d82
coralseu <coralseu@163.com>
2018-02-16 23:12:21 +0800

    Update talks

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,21 +1,16 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
+    "Washington D.C., US",
     37.8708393,
     -122.2728638
   ],
   [
-    "London, UK",
-    51.5073219,
-    -0.1276473
-  ],
-  [
-    "San Francisco, California",
+    "Beijing, China",
     37.7792808,
     -122.4192362
   ],
   [
-    "Los Angeles, CA",
+    "¶«Ý¸, ÖÐ¹ú",
     34.0543942,
     -118.2439408
   ]
